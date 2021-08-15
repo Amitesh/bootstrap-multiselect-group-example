41a6bcd8cc41fe80b69704db093843673fda4a51
Amitesh <amitesingh@gmail.com>
2021-08-15 17:06:18 +0530

    3 files modified

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-console.log('hello!');
-alert(123);
+$(document).ready(function() {
+  $('#example-enableCollapsibleOptGroups').multiselect({
+    enableCollapsibleOptGroups: true
+  });
+
+  $('#example-enableCollapsibleOptGroups-new').multiselect({
+    enableCollapsibleOptGroups: true
+  });
+
+  enhanceCollapsibleButton();
+});
+
+function enhanceCollapsibleButton() {
+  $(
+    '#example-enableCollapsibleOptGroups-new .multiselect-container .caret-container'
+  ).addClass('open');
+  $(
+    '#example-enableCollapsibleOptGroups-new .multiselect-container .caret-container'
+  ).click(function() {
+    console.log(this);
+    $(this).toggleClass('open close');
+  });
+}
